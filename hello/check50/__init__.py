d4dd31f5dd9a7c93afb248add02c38dd43dae086
CaryACTvF <31970190+CaryACTvF@users.noreply.github.com>
2019-06-29 13:30:41 -0400

    Update __init__.py

--- hello/check50/__init__.py
+++ hello/check50/__init__.py
@@ -6,5 +6,10 @@ class Similarities(Checks):
 
     @check()
     def exists(self):
-        """helpers.py exists"""
+        """hello.py exists"""
         self.require("hello.py")
+        
+    @check("compiles")
+    def hello(self):
+        """prints "Hello world!" """
+        self.spawn("python3 ./hello.py").stdout("Hello world!", "Hello world!")
